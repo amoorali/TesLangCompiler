6427a8ace2a0550266acc5a02690718c4e73352c
amrali <amirfallahi222@gmail.com>
2022-11-18 23:33:03 +0330

    initial lexer file

--- lexer.py
+++ lexer.py
@@ -1,2 +1,10 @@
+import re
+
+IDENTIFIER = r'[A-Za-z_][A-Za-z_0-9]*'
+NUMBER = r'[0-9]+'
+COMMENT = r'(\/\/)(.*?)[\n\r]'
+
+
 def getTokens(code_string: str) -> list:
+    # re.search(r'[]')
     pass
