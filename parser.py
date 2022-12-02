59d77e0f73634fa4714ec56c46f362a5de402286
amrali <amirfallahi222@gmail.com>
2022-12-02 23:43:20 +0330

    removed parser.py

--- parser.py
+++
@@ -1,9 +0,0 @@
-from tools.parser_generator import ParserGenerator
-
-
-class Parser():
-    def __init__(self) -> None:
-        self.parser = ParserGenerator()
-
-    def get_parser():
-        pass
